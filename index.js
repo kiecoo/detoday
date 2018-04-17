7d39be305f492f44e36414141c4a554858e4e615
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-17 19:54:17 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+// index.js
+
+var makeLinkUrl = require('./make-gitter-url.js')  
+
+module.exports = makeGitterChat
+
+ function makeGitterChat(username){
+       return bel`
+   
+      <div class=${css.cardGitterChat} ><iframe class=${css.iframe} src=${ makeLinkUrl (username)}></iframe></div>
+      `
+ }

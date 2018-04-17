59fb48a83d3a5f571d23c007c49932ca86d9bfda
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-17 19:40:57 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+// in 【index.js】
+
 module.exports = makeGitterURL
 
 function makeGitterURL (username) {
   return `https://gitter.im/${username}/~embed`
 }
+
+	        //difference from the repo of makeGitterChatbox https://github.com/kiecoo/makeGitterChatbox/blob/master/index.html
+//		var x = makeGitterChatbox(exampleProfile)
+//		document.body.appendChild(x)

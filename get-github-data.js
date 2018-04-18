9dbcf28c253d74e28896398471bc2e2d278ed35d
Michelle <34873832+kiecoo@users.noreply.github.com>
2018-04-18 15:34:07 +0800

    Create get-github-data.js

---
+++ get-github-data.js
@@ -0,0 +1,23 @@
+// get-github-data.js
+
+module.exports = getGithubData
+
+function getGithubData () {
+
+    var data= [
+    {
+        "repo": "nicco88/personal-portfolio",
+        "url": "https://api.github.com/repos/nicco88/personal-portfolio",
+        "username": "ninabreznik",
+        "time": "2018-04-01T17:50:29Z"
+    },
+    {
+        "repo": "freeCodeCamp/devdocs",
+        "url": "https://api.github.com/repos/freeCodeCamp/devdocs",
+        "username": "serapath",
+        "time": "2018-03-27T04:51:07Z"
+    }]
+    
+    
+    return  data
+  }
